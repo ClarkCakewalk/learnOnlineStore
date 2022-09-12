c087f3eea316c8b6ed73145a62f8857d17a14fb1
Clark in MBA <clark@alum.ccu.edu.tw>
2022-09-12 21:54:03 +0800

    ch18

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     @foreach ($viewData["products"] as $product)
     <div class="card">
-        <img src="{{ asset('/img/'.$product->getImage())}}" class="card-img-top">
+        <img src="{{ asset('/storage/'.$product->getImage())}}" class="card-img-top">
         <div class="card-body text-center">
             <a href="{{route('product.show', ['id'=>$product->getId()])}}" class="btn bg-primary text-white">{{$product->getName()}}</a>
 </div>
